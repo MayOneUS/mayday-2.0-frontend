fe2c2e2ce99d06fc59a93c0137163df7504f42f6
Rio Flores <rio517@gmail.com>
2015-04-28 15:57:22 -0400

    Legislators update

--- js/homepage.js
+++ js/homepage.js
@@ -10,7 +10,7 @@ $(function() {
     api_endpoint = 'bills/supporter_counts',
     bill_url_hr424 = root_services_url + api_endpoint + '?bill_id=hr424-114',
     bill_url_hr20 = root_services_url + api_endpoint + '?bill_id=hr20-114',
-    all_supporters_url = '//mayday-step-two.herokuapp.com/' + api_endpoint,
+    all_supporters_url = root_services_url + api_endpoint,
 
     // dom objects to render to
     $container1 = $('#container1'),
