76de8fb5307de7ff27630e273fbca559391d7d4b
Rio Flores <rio517@gmail.com>
2015-06-19 14:57:06 -0400

    JS: remove unused code

--- _assets/javascripts/bases/index.js
+++ _assets/javascripts/bases/index.js
@@ -4,15 +4,4 @@ if( window.location.pathname == '/' ){
     window.location = '/june23/';
   })
 
-  $('.js-sign-letter-form').validate({
-    submitHandler: function(form) {
-      $form = $(form);
-      $form.find('button.btn').html('<i class="fa fa-refresh fa-spin"></i> Loading...');
-      $.post('{{site.services_url}}/actions', $form.serialize(), function(data){
-      }).done(function(){
-        window.location = '/sign-the-letter/?already_signed=true';
-      });
-    }
-  });
-
 }
