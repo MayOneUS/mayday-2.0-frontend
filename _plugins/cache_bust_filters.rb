51692898e2764f53c9b35a4ce8de71c099b8f86a
Rio Flores <rio517@gmail.com>
2015-12-14 14:26:17 +0100

    Rename plugin file, add js_cache_bust

--- _plugins/cache_bust_filters.rb
+++ _plugins/cache_bust_filters.rb
@@ -1,7 +1,7 @@
+# Note: Github doesn't allow plugins to execute (safemode). These filters won't work on github pages.
 module Jekyll
-  module MyFilters
-    # Note: Github doesn't allow plugins to execute (safemode). This plugin won't work on github pages.
-    #
+  module CacheBustFilters
+
     # Gets Md5 contents of target file (assumed to be using the full path)
     # and appends a hash end of to asset file reference. Ensures deployed
     # asset files are "cachebust-ed" every time the file changes
@@ -17,9 +17,19 @@ module Jekyll
       require 'digest/md5'
       file_contents = Dir.glob("_sass/*").map{|f| File.read(f) }.join
       file_name + '?' + Digest::MD5.hexdigest(file_contents)
+    end
 
+    # Gets Md5 contents of all js assets in _assets/javascripts/
+    # and appends a hash end of the target asset file. Ensures deployed
+    # files are "cachebust-ed" every time the files change. Using
+    # jekyll-assests requires first capturing the asset_path string, and
+    # passing the path to this filter in a subsequent liquid call
+    def js_cache_bust(file_name)
+      require 'digest/md5'
+      file_contents = Dir.glob("_assets/javascripts/**/*").map{|f| File.read(f) unless File.directory?(f) }.join
+      file_name + '?' + Digest::MD5.hexdigest(file_contents)
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::MyFilters)
+Liquid::Template.register_filter(Jekyll::CacheBustFilters)
