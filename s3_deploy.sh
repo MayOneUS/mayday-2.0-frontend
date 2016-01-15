6a8433aef7e361163ce5778b8770bf0db8ce6d45
Rio Flores <rio517@gmail.com>
2016-01-15 11:15:52 +0100

    Config: Use custom envioronments to establish which services url to use

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -5,18 +5,21 @@
 # Determine which bucket we should deploy to
 if [[ "$TRAVIS_TAG" =~ 'alpha' ]]; then
     echo "Going to deploy to alpha.mayday.us"
+    environment='alpha'
     export S3_BUCKET=alpha.mayday.us
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Going to deploy to beta.mayday.us"
+    environment='production'
     export S3_BUCKET=beta.mayday.us
 elif [ "$TRAVIS_BRANCH" = "production" ]; then
     echo "Going to deploy to mayday.us"
+    environment='production'
     export S3_BUCKET=mayday.us
 else
     echo "This branch isn't alpha, master, or production - not deploying anything"
     exit 0
 fi
 
-JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=$environment jekyll build
 s3_website cfg apply --headless
 s3_website push
